b42c1574f4fe49570acb0e69ac00259b14748bc9
Steven Lamont <Steve@Lamont.email>
2015-08-17 10:10:19 -0700

    test 2 of commit from school computer

--- HelloWorld/Hello_World.java
+++ HelloWorld/Hello_World.java
@@ -9,6 +9,6 @@ public class Hello_World
 {
      public static void main(String[] args)
     {
-        System.out.println("Hello World! From Stevie"); //Display the string
+        System.out.println("Hello World! From Stevie, round 2"); //Display the string
     }
 }
