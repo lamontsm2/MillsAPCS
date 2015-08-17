f316e23a1c1fa4c24cdcd215401d39d0d56819f1
Steven Lamont <Steve@Lamont.email>
2015-08-17 10:05:18 -0700

    test of commit on school computer

---
+++ HelloWorld/Hello_World.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Hello_World here.
+ * 
+ * @author Steve 
+ * @version (a version number or a date)
+ */
+public class Hello_World
+{
+     public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Stevie"); //Display the string
+    }
+}
